373465bbb8f659ec6b5d6b4615a0b6267b5e71f2
shimeng <shimeng@maoyan.com>
2019-07-16 19:23:46 +0800

    replace hash history

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Menu } from 'antd';
 import {
-  BrowserRouter as Router, Link,
+  HashRouter as Router, Link,
   Route, Switch,
 } from 'react-router-dom';
 import Home from './hooks/Home';
